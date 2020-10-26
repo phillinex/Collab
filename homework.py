c8c988364f9769fa030143aa4309c547a9b1dcce
Philip Asumah <phillinex@outlook.com>
2020-10-26 23:30:17 +0300

    A look into the code

--- homework.py
+++ homework.py
@@ -1,6 +1,11 @@
 a=['apple','orange','banana','pear','raspberry','peach','plum']
 fruit=input("Enter name of the fruit:")
-if fruit in a:
+if fruit in a:    
     print("There")
 else:
     print("Not there")
+
+#If there are 10 million fruits in the list and the user is wants to know if 'apple' is in the first, 
+# fortunately, apple happens to be the first fruit in the list. Now ask yourself, after the code finds 
+# apple early on in the list, will it continue to search through the remain items in the list or it 
+# will pause there. Think about it, find out what your code does and let me know.
