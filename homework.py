f8d2c6a860fbc39c67180af5a31fda7ac1a49c8e
Philip Asumah <phillinex@outlook.com>
2020-10-27 04:26:24 +0300

    corrections

--- homework.py
+++ homework.py
@@ -5,7 +5,7 @@ if fruit in a:
 else:
     print("Not there")
 
-#If there are 10 million fruits in the list and the user is wants to know if 'apple' is in the first, 
+# If there are 10 million fruits in the list and the user wants to know if 'apple' is in the fruits list, 
 # fortunately, apple happens to be the first fruit in the list. Now ask yourself, after the code finds 
 # apple early on in the list, will it continue to search through the remain items in the list or it 
-# will pause there. Think about it, find out what your code does and let me know.
+# will stop there and print out 'There'. Think about it, find out what your code does and let me know.
